aea0d2662012ae9064e1259c69efbcee1606c896
Prajyot Narulkar <prajyot.narulkar@ibm.com>
2022-11-25 12:41:42 +0530

    aws-ec2

---
+++ main.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+  region     = "ap-south-1"
+  access_key = ""
+  secret_key = ""
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-074dc0a6f6c764218"
+  instance_type = "t2.micro"
+  name          = "gitops-deployment"
+}
