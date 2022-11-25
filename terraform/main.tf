30a90cae4300bab6995e14ceacd0848f5cc3b6d7
Prajyot Narulkar <76838576+prajyot-narulkar925@users.noreply.github.com>
2022-11-25 16:55:37 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "4.41.0"
+    }
+  }
+}
+
 variable "access_key" {
    type = string
    default = ""
