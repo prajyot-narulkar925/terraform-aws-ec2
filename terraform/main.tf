735bdec4c372f1554d5dcdbce491e9df58d342b1
Prajyot Narulkar <prajyot.narulkar@ibm.com>
2022-11-29 11:57:05 +0530

    changes

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,8 +9,8 @@ terraform {
 
 provider "aws" {
   region     = "ap-south-1"
-  access_key = "${var.aws_access_key}"
-  secret_key = "${var.aws_secret_key}"
+  access_key = var.access_key
+  secret_key = var.secret_key
 }
 
 resource "aws_instance" "example" {
